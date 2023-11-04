4a57b63ef38fac5821e5728b3f8cf10fd354299c
Rayansh <rayanshsrivastava1@gmail.com>
2023-11-05 03:46:16 +0530

    push

--- app.py
+++ app.py
@@ -5,8 +5,6 @@ import requests
 import os
 from transformers import AutoTokenizer, AutoModelForCausalLM
 from IPython.display import Audio
-from PIL import Image
-import io
 
 st.title("Image to Audio Text Generation")
 
@@ -19,13 +17,7 @@ def img2text(url):
     st.text("Generated Text from Image:")
     st.write(text)
 
-    # Display the selected image
     st.image(url, use_column_width=True)
-
-    # Display the URL as plain text with a copy option
-    st.write("Image URL:")
-    st.text(url)
-
     return text
 
 # Get the image URL from the user
@@ -34,7 +26,7 @@ if st.button("Generate Text from Image"):
     image_caption = img2text(image_url)
 
     API_URL = "https://api-inference.huggingface.co/models/HuggingFaceH4/zephyr-7b-beta"
-    headers = {"Authorization": "Bearer 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"}
+    headers = {"Authorization": f"Bearer {HUGGINGFACEHUB_API_TOKENS}"}
 
     def query(prompt, max_new_tokens=200):
         payload = {
@@ -49,8 +41,6 @@ if st.button("Generate Text from Image"):
     data = query(image_caption, max_new_tokens=250)
     generated_text = data[0].get("generated_text", "")
     st.text("Generated Text:")
-
-    # Display the generated text
     st.write(generated_text)
 
     API_URL = "https://api-inference.huggingface.co/models/espnet/kan-bayashi_ljspeech_vits"
@@ -64,12 +54,9 @@ if st.button("Generate Text from Image"):
         # Create an Audio object to play the binary audio data
         return Audio(audio, rate=sampling_rate)
 
+    # if st.button("Generate and Play Audio"):
     text_to_speak = generated_text
     audio_object = generate_and_play_audio(text_to_speak)
     st.write("Audio Response:")
-
-    # Display the selected image again
-    st.image(image_url, use_column_width=True)
-
-    # Display the generated audio
     st.audio(audio_object.data, format="audio/wav")
+
